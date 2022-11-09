820cdc2d4bed59bdef52db198d8598fcd931f1af
Negar Rezaei <N.Rezaei@pishgam.local>
2022-11-09 17:21:52 +0330

    add button cart

--- cart.js
+++ cart.js
@@ -1,8 +1,8 @@
-// let modal = document.getElementById("modal");
+let modal = document.getElementById("modal");
 // let data = document.getElementById("par").value;
 // let par = document.querySelector('#par');
 // const innerInput = document.getElementById("par").value;
-
+let addToCartButton  = "";
 let items = "";
 // let inputt = "";
 const shoppingBaskeItems = [];
@@ -10,7 +10,7 @@ const shoppingBaskeItems = [];
 // localStorage.setItem('name' , JSON.stringify(productNames));
 // localStorage.setItem('image' , JSON.stringify(images));
 
-shoppingBaskeItems.splice(shoppingBaskeItems.length - 4);
+// shoppingBaskeItems.splice(shoppingBaskeItems.length - 4);
 
 emptyModal();
 
@@ -20,7 +20,7 @@ function emptyModal() {
       ' <p class="modaltext mt-5">سبد خرید شما خالی است!</p><p class="modaltext2">می‌توانید برای مشاهده محصولات بیشتر به صفحه <a href="/cards-with-sass">محصولات</a> بروید.</p>';
   }
 }
-creatItems();
+createItems();
 
 function onClickAddBtn(name, image, price, value, productId) {
   const obj = {
@@ -34,31 +34,37 @@ function onClickAddBtn(name, image, price, value, productId) {
   // inputNumber: data,
   emptyModal();
   shoppingBaskeItems.push(obj);
-  creatItems();
+  createItems();
   input(productId);
+  
   // increment(value,productId);
 }
 
-function creatItems() {
+function createItems() {
   items = "";
+
   for (let product of shoppingBaskeItems) {
+    let productId = product.id;
+    let image = product.images;
+    let productName = product.productNames;
+    let productPrice = product.productPrice;
     items += `
         <div class="shopping_cart_item" id="shopping_cart">
         <div class="d-flex flex-row align-items-center justify-content-between pt-2">
-            <div
+            <div 
                 class="cart_text_item d-flex flex-nowrap justify-content-center align-items-center">
-                <img src="${product.images}" alt="mobile"
+                <img src="${image}" alt="mobile"
                     class="logo pl-2">
-                <p class="shopping_cart_text">${product.productNames}</p>
+                <p class="shopping_cart_text">${productName}</p>
             </div>
-            <p class="shopping_cart_text">${product.productPrice}</p>
+            <p class="shopping_cart_text">${productPrice}</p>
             <div
                 class="shopping_cart_button d-flex justify-content-center align-items-center">
-                <button type="button" class="btn btn-outline-info ml-2"  onclick="incrementCart(${product.id})">+</button>
+                <button type="button" class="btn btn-outline-info ml-2"  onclick="incrementCart('${productId}')">+</button>
                 <input type="text" class="input_number"  value="1">
-                <button type="button" class="btn btn-outline-info mr-2" onclick="incrementCart(${product.id})">-</button>
+                <button type="button" class="btn btn-outline-info mr-2" onclick="decrementCart('${productId}')">-</button>
             </div>
-            <button class=" btn-danger btn-sm float-right mr-3 delete"   onclick="onClickDeleteBtn('${product.productNames}')">X</button>
+            <button class=" btn-danger btn-sm float-right mr-3 delete"   onclick="onClickDeleteBtn('${productId}')">X</button>
         </div>
         </div>
         `;
@@ -66,7 +72,11 @@ function creatItems() {
     // localStorage.setItem('tasks' ,  JSON.stringify(tasks) );
   }
 
+  // let div = document.getElementById('item_443');
+
+  // var div = document.createElement('div');
   modal.innerHTML = items;
+
   // getAllTask(items);
 }
 function input(productId) {
@@ -89,6 +99,19 @@ function input(productId) {
   let button = card.querySelector(".btn");
   let div = document.createElement("div");
   div.innerHTML = inputt;
+  button.before(div);
+  button.remove();
+}
+function buttonCard(productId) {
+
+  addToCartButton = "";
+  addToCartButton = `<a href="#" class="btn btn-primary"  onclick="onClickAddBtn()" >
+  افزودن به سبد خرید
+  </a>`;
+  let card = document.getElementById(productId);
+  let button = card.querySelector(".shopping_cart_button");
+  let div = document.createElement("div");
+  div.innerHTML = addToCartButton;
   button.before(div);
   button.remove();
 }
@@ -116,14 +139,28 @@ function decrement(productId) {
     input.value = product.count;
   } else if (input.value < 2) {
     removeObjectWithId(shoppingBaskeItems, productId);
+    buttonCard(productId) ;
+  }
+}
+function removeObjectWithId(arr, id) {
+  const objWithIdIndex = arr.findIndex((p) => p.id === id);
+  if (confirm("کالا از سبد خرید شما حذف خواهد شد.آیا مطمئن هستید؟")) {
+    arr.splice(objWithIdIndex, 1);
+    const cartItem = document.getElementById("shopping_cart");
+    cartItem.remove();
+  }
+  emptyModal();
+}
+function onClickDeleteBtn() {
+  if (confirm("کالا از سبد خرید شما حذف خواهد شد.آیا مطمئن هستید؟")) {
+    const cartItem = document.getElementById("shopping_cart");
+    cartItem.remove();
   }
 }
 // function incrementCart(productId) {
-//   // let cart = document.getElementsByClassName("shopping-cart");
-//   // let inputCart = cart.querySelector(".input_number");
-//   let inputCart = document
-//     .getElementById("shopping_cart")
-//     .querySelectorAll("input");
+//   debugger
+//   let cart = document.getElementsByClassName("shopping-cart");
+//   let inputCart = cart.querySelector(".input_number");
 //   let currentValue = inputCart.value.split("")[0];
 //   const product = shoppingBaskeItems.find((p) => {
 //     return p.id === productId;
@@ -144,26 +181,6 @@ function decrement(productId) {
 //     inputCart.value = product.count;
 //   }
 // }
-function removeObjectWithId(arr, id) {
-  const objWithIdIndex = arr.findIndex((p)=>p.id === id);
-  if (confirm("کالا از سبد خرید شما حذف خواهد شد.آیا مطمئن هستید؟")) {
-    arr.splice(objWithIdIndex, 1);
-    const cartItem = document.getElementById('shopping_cart');
-    cartItem.remove();
-  }
-  
-}
-// function addCartBotton(productId) {
-//   let button = `
-//   <a href="#" class="btn btn-primary" >افزودن به سبد خرید</a>
-//    `;
-//   let card = document.getElementById(productId);
-//   let button = card.querySelector("input");
-//   let div = document.createElement("div");
-//   div.innerHTML = buttton;
-//   button.before(div);
-//   button.remove();
-// }
 
 // function incrementCart(value) {
 //   let input = document.getElementById("parCart");
@@ -179,20 +196,6 @@ function removeObjectWithId(arr, id) {
 //   }
 // }
 
-function onClickDeleteBtn() {
-  if (confirm("کالا از سبد خرید شما حذف خواهد شد.آیا مطمئن هستید؟")) {
-  const cartItem = document.getElementById('shopping_cart');
-  cartItem.remove();
-  emptyModal();
-}
-
-  // const objWithIdIndex = arr.findIndex((p)=>p.id === id);
-  // if (confirm("کالا از سبد خرید شما حذف خواهد شد.آیا مطمئن هستید؟")) {
-  //   arr.splice(objWithIdIndex, 1);
-  //   const cartItem = document.getElementById();
-  //   cartItem.remove();
-  // }
-}
 // document.addEventListener('DOMContentLoaded' , function (e)  {
 //     let tasks;
 

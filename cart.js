74f494a0f506c3b29b2a41aa6e99aca29cf8ec69
Negar Rezaei <N.Rezaei@pishgam.local>
2022-11-13 19:05:18 +0330

    add localstorage

--- cart.js
+++ cart.js
@@ -1,31 +1,24 @@
 let modal = document.getElementById("modal");
-const cartCount = document.querySelector(".cart_count");
+let cartCount = document.querySelector(".cart_count");
 let addToCartButton = "";
 let items = "";
-const shoppingBaskeItems = [];
-// localStorage.setItem('storage' ,  JSON.stringify(productPrice) );
-// localStorage.setItem('name' , JSON.stringify(productNames));
-// localStorage.setItem('image' , JSON.stringify(images));
+var shoppingBaskeItems = [];
 
 
-emptyModal();
 
-// var oldItems = JSON.parse(localStorage.getItem('itemsArray')) || [];
 
-// oldItems.push(shoppingBaskeItems);
-
-// localStorage.setItem('itemsArray', JSON.stringify(oldItems));
-
+// cartCount.innerText = shoppingBaskeItems.length;
 
 function emptyModal() {
+  console.log(shoppingBaskeItems);
   if (shoppingBaskeItems.length == 0) {
     modal.innerHTML =
       ' <p class="modaltext mt-5">سبد خرید شما خالی است!</p><p class="modaltext2">می‌توانید برای مشاهده محصولات بیشتر به صفحه <a href="/cards-with-sass">محصولات</a> بروید.</p>';
   }
 }
 
-function onClickAddBtn(name, image, price, value, productId) {
-  const obj = {
+function onClickAddBtn(name, image, price, count, productId) {
+let obj = {
     id: productId,
     productNames: name,
     images: image,
@@ -35,9 +28,10 @@ function onClickAddBtn(name, image, price, value, productId) {
   emptyModal();
   shoppingBaskeItems.push(obj);
   cartCountNumber();
-  
   createItems();
-  input(productId);
+  input(productId,count);
+ setInLocalStorage();
+  
 
 }
 
@@ -49,6 +43,7 @@ function createItems() {
     let image = product.images;
     let productName = product.productNames;
     let productPrice = product.productPrice;
+    let count = product.count;
     items += `
         <div class="shopping_cart_item" id="shopping_${productId}">
         <div class="d-flex flex-row align-items-center justify-content-between pt-2">
@@ -62,7 +57,7 @@ function createItems() {
             <div
                 class="shopping_cart_button d-flex justify-content-center align-items-center">
                 <button type="button" class="btn btn-outline-info ml-2"  onclick="increment('${productId}')">+</button>
-                <input type="text" class="input_number"  value="1" >
+                <input type="text" class="input_number"  value="${count}" >
                 <button type="button" class="btn btn-outline-info mr-2" onclick="decrement('${productId}')">-</button>
             </div>
             <button class=" btn-danger btn-sm float-right mr-3 delete"   onclick="onClickDeleteBtn('${productId}')">X</button>
@@ -71,15 +66,15 @@ function createItems() {
         `;
   }
   modal.innerHTML = items;
-// window.localStorage.setItem("myObject", JSON.stringify(items));
+// window.localStorage.setItem("myObject", JSON.stringify(shoppingBaskeItems));
 // modal.innerHTML = window.localStorage.getItem("myObject");
 
-window.localStorage.setItem("myObject", JSON.stringify(items));
-let newObject = window.localStorage.getItem("myObject");
-console.log(JSON.parse(newObject));
+// window.localStorage.setItem("myObject", JSON.stringify(shoppingBaskeItems));
+// let newObject = window.localStorage.getItem("myObject");
+// console.log(JSON.parse(newObject));
   // getAllTask(items);
 }
-function input(productId) {
+function input(productId,count) {
   let inputt = `
   <div class="shopping_cart_button d-flex justify-content-center align-items-center onclick="cartCountNumber()" >
 
@@ -87,7 +82,7 @@ function input(productId) {
    +
    </button>
 
-   <input type="text" class="input_number"  value="1" >
+   <input type="text" class="input_number"  value="${count} " >
 
    <button type="button" class="btn btn-outline-info mr-2" onclick="decrement('${productId}')">
    -
@@ -163,7 +158,7 @@ function increment(productId) {
   // cartCount.innerText = product.count;
 }
 function cartCountNumber() {
-  cartCount.innerText++;
+  cartCount.innerText = shoppingBaskeItems.length;
 }
 function cartCountNumberr() {
 
@@ -219,94 +214,48 @@ function onClickDeleteBtn(productId) {
     buttonCard(productId);
     cartCountNumberr();
     emptyModal();
-
+    deleteLocalStorage();
   }
 
 }
-// function incrementCart(productId) {
-//   let card = document.getElementById("shopping_"+ productId);
-//   let input = card.querySelector(".input_number");
-//   let currentValue = input.value.split("")[0];
-//   const product = shoppingBaskeItems.find((p) => {
-//     return p.id === productId;
-//   });
-//   product.count = +currentValue + 1;
-//   input.value = product.count;
-//   cartCount.innerText = product.count;
-// }
+// (function () 
+//   {
+//     // let objItems = JSON.stringify(shoppingBaskeItems);
+//     // window.localStorage.setItem("myObject", objItems);
+//     // let x = window.localStorage.getItem("myObject");
+//     // console.log(x);
 
-// function decrementCart(productId) {
-//   let inputCart = cart.querySelector(".input_number");
-//   let currentValue = inputCart.value.split("")[0];
-//   if (inputCart.value >= 1) {
-//     const product = shoppingBaskeItems.find((p) => {
-//       return p.id === productId;
-//     });
-//     product.count = +currentValue - 1;
-//     inputCart.value = product.count;
+//     window.localStorage.setItem("myObject", JSON.stringify(shoppingBaskeItems));
+// let newObject = window.localStorage.getItem("myObject");
 //   }
-// }
+// )
+// ();
 
-// function incrementCart(value) {
-//   let input = document.getElementById("parCart");
-//   let currentValue = input.value.split("")[0];
-//   input.value = +currentValue + value + "";
-// }
 
-// function decrementCart(value) {
-//   let input = document.getElementById("parCart");
-//   if (input.value >= 1) {
-//     let currentValue = input.value.split("")[0];
-//     input.value = +currentValue + value;
-//   }
-// }
 
-// document.addEventListener('DOMContentLoaded' , function (e)  {
-//     let tasks;
+// // function deleteLocalStorage(){
+// //   localStorage.removeItem("myObject");
+// // }
 
-//     if (localStorage.getItem('tasks') === null) {
-//       tasks = [];
-//     } else {
-//       tasks = localStorage.getItem('tasks').split(',');
-//     }
-//     for(let item of tasks)
-//       item = "";
-//       for (let index = 0; index < productNames.length; index++) {
-//         item += `
-//         <div class="shopping_cart_item">
-//         <div class="d-flex flex-row align-items-center justify-content-between pt-2">
-//             <div
-//                 class="cart_text_item d-flex flex-nowrap justify-content-center align-items-center">
-//                 <img src="${images[index]}" alt="mobile"
-//                     class="logo pl-2">
-//                 <p class="shopping_cart_text">${productNames[index]}</p>
-//             </div>
-//             <p class="shopping_cart_text">${productPrice[index]}</p>
-//             <div
-//                 class="shopping_cart_button d-flex justify-content-center align-items-center">
-//                 <button type="button" class="btn btn-outline-info ml-2"  onclick="incrementCart(+1)">+</button>
-//                 <input type = "text" class = "input_number" id = "parCart" value = "0" onchange = "myFunction()">
-//                 <button type ="button" class ="btn btn-outline-info mr-2" onclick ="decrementCart(-1)">-</button>
-//             </div>
-//             <button class=" btn-danger btn-sm float-right mr-3 delete"   onclick="onClickDeleteBtn('${productNames[index]}')">X</button>
-//         </div>
-//         </div>
-//         `;}
-//          modal.innerHTML = items;
-// })
-// function getAllTask(name , image , price) {
-//   let tasks;
+function  setInLocalStorage(){
+  localStorage.setItem("myObject", JSON.stringify(shoppingBaskeItems));
+  // let newObject = window.localStorage.getItem("myObject");
+}
+(function(){
+  cartCountNumber();
+  const items= JSON.parse( localStorage.getItem("myObject"));
 
-//   if (localStorage.getItem('tasks') === null) {
-//     tasks = [];
-//   } else {
-//     tasks = localStorage.getItem('tasks').split(',');
-//   }
-//   tasks.push(name, image, price);
-//   localStorage.setItem('tasks', name,image,price);
-// }
+  if (items) {
+    shoppingBaskeItems=items;
+    createItems();
+  }
+})();
 
-// function myFunction() {
-//   var mylist = document.getElementById("parCart");
-//   document.getElementById("par").value = mylist[mylist.selectedIndex].text;
-// }
+function deleteLocalStorage(){
+  
+  localStorage.removeItem("myObject");
+}
+
+function cartCountNumber(){
+  cartCount.innerText = shoppingBaskeItems.length;
+  }

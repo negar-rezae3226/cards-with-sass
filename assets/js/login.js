8bd4380ed5d349cefb69678c1232a2cbf3db6ef0
Negar Rezaei <N.Rezaei@pishgam.local>
2022-11-27 10:41:56 +0330

    update login form

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,37 +1,47 @@
 let allUsers;
-let login =  document.getElementById("login-form");
-let username = document.getElementById("username").value;
-let password = document.getElementById("password").value;
-let email = document.getElementById("email").value;
+let login = document.getElementById("login-form");
+// let username = document.getElementById("username").value;
+// let password = document.getElementById("password").value;
+// let email = document.getElementById("email");
+
+//#region API
 
 getAllUsers();
 
 function getAllUsers() {
-    fetch('https://dummyjson.com/users')
-      .then((res) => res.json())
-      .then((json) => {
-        allUsers = json.users;
-        console.log(allUsers);
-      });
-  }
-
-  login.addEventListener("submit", auth);
-
-function auth() {
-
-
-     console.log(username);
-     allUsers.forEach((users) => {
-
-     if (username == `${users.username}`  && password == "111" && email == "admin@gmail.com") {
-        alert("login successful");
-     }  
-     if (username === "" && password === "") {
-          alert("Please enter information");
-     } 
-     else{
-         alert("Please enter valid information");
-         return;
-     }
+  fetch("https://dummyjson.com/users")
+    .then((res) => res.json())
+    .then((json) => {
+      allUsers = json.users;
+      console.log(allUsers);
     });
 }
+
+//#endregion
+
+//#region login
+
+function loginUser() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let email = document.getElementById("email").value;
+
+  for (let index = 0; index < allUsers.length; index++) {
+
+    if (username === allUsers[index].username && password === allUsers[index].password && email === allUsers[index].email) {
+      
+      alert("ورود شما با موفقیت انجام شد :) ");
+
+      return;
+    } 
+    if (username === "" ) {
+      alert('fgbgfg')
+    }
+    else {
+      alert("کاربر با این اطلاعات در سیستم ثبت نشده است");
+      return;
+    }
+  }
+}
+
+//#endregion
